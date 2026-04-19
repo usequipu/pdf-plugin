be73d2ca18da673d7c3f5551e3c5d77e53b34721
barelias <iago@visio.ai>
2026-04-19 18:48:12 -0300

    fix: ESM with external React, proxy import shim, process polyfill v0.1.2

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,19 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+    'process.browser': 'true',
+    'process.version': JSON.stringify(''),
+  },
   build: {
     lib: {
       entry: 'src/index.tsx',
-      formats: ['cjs'],
+      formats: ['es'],
       fileName: () => 'index.js',
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime'],
+      external: ['react', 'react-dom', 'react/jsx-runtime', 'react-dom/client'],
       output: {
         inlineDynamicImports: true,
       },
